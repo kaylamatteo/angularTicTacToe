18a9a688dbd5e1baf188d05944e3fc72435cb72c
Kayla Matteo <kaylamatteo@gmail.com>
2016-03-02 17:21:55 -0600

    non functional board

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,23 @@
-angular.module('tictactoeApp', []);
+angular.module('tictactoeApp', [])
+
+.controller('mainCtrl', function($scope){
+
+	$scope.reset = function() {
+		console.log("reset function ran")
+	}
+
+	$scope.spaces = [
+	{"position": "1a"},
+	{"position": "1b"},
+	{"position": "1c"},
+	{"position": "2a"},
+	{"position": "2b"},
+	{"position": "2c"},
+	{"position": "3a"},
+	{"position": "3b"},
+	{"position": "3c"},
+	]
+
+	$scope.currentPlayer = "X"
+})
+
