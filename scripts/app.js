3ab1f8acbe5526748f8d9db3fff9b7a259d122b4
Kayla Matteo <kaylamatteo@gmail.com>
2016-03-03 13:56:49 -0600

    simplified by using player letter as values instead of numbers

--- scripts/app.js
+++ scripts/app.js
@@ -3,48 +3,38 @@ angular.module('tictactoeApp', [])
 .controller('mainCtrl', function($scope){
 	$scope.currentPlayer = "X";
 
-	$scope.reset = function() {
-		console.log("reset function ran");
-	}
-
 	$scope.spaces = [
-	{"position": "1a", "text": " ", "value": 0},
-	{"position": "1b", "text": " ", "value": 0},
-	{"position": "1c", "text": " ", "value": 0},
-	{"position": "2a", "text": " ", "value": 0},
-	{"position": "2b", "text": " ", "value": 0},
-	{"position": "2c", "text": " ", "value": 0},
-	{"position": "3a", "text": " ", "value": 0},
-	{"position": "3b", "text": " ", "value": 0},
-	{"position": "3c", "text": " ", "value": 0},
+	{"position": "1", "value": " "},
+	{"position": "2", "value": " "},
+	{"position": "3", "value": " "},
+	{"position": "4", "value": " "},
+	{"position": "5", "value": " "},
+	{"position": "6", "value": " "},
+	{"position": "7", "value": " "},
+	{"position": "8", "value": " "},
+	{"position": "9", "value": " "},
 	]
 
 	$scope.changePlayer = function(space) {
-		if ($scope.currentPlayer === "X" && space.value === 0){
+		if ($scope.currentPlayer === "X" && space.value === "X"){
 			$scope.currentPlayer = "O";
-		} else if (!space.value) {
+		} else if (space.value === "O" && space.value === "O") {
 			$scope.currentPlayer = "X";
 		}
 	}
 
 	$scope.makeMove = function(space) {
-		if(!space.value){
+		if(space.value === " "){
 			console.log("running:", $scope.currentPlayer);
-			space.text = $scope.currentPlayer;
+			space.value = $scope.currentPlayer;
 		}
 	}
 
-	$scope.setPlayerValue = function(space) {
-		if(space.text === "X"){
-			space.value = 1;
-			console.log("space.value = ", space.value);
-		}
-		else if(space.text === "O"){
-			space.value = 2;
-			console.log("space value is now = ", space.value);
-		}
+	$scope.reset = function() {
+		console.log($scope.spaces.value);
+		$scope.spaces.value = " ";
+		console.log($scope.spaces.value);
 	}
-
 
 })
   
