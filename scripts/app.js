c5a118470e1edcc6bd36b14824d241ad7d7f57fc
Kayla Matteo <kaylamatteo@gmail.com>
2016-03-03 12:20:54 -0600

    X and O space marking functionality added

--- scripts/app.js
+++ scripts/app.js
@@ -8,15 +8,15 @@ angular.module('tictactoeApp', [])
 	}
 
 	$scope.spaces = [
-	{"position": "1a"},
-	{"position": "1b"},
-	{"position": "1c"},
-	{"position": "2a"},
-	{"position": "2b"},
-	{"position": "2c"},
-	{"position": "3a"},
-	{"position": "3b"},
-	{"position": "3c"},
+	{"position": "1a", "text": " ", "value": 0},
+	{"position": "1b", "text": " ", "value": 0},
+	{"position": "1c", "text": " ", "value": 0},
+	{"position": "2a", "text": " ", "value": 0},
+	{"position": "2b", "text": " ", "value": 0},
+	{"position": "2c", "text": " ", "value": 0},
+	{"position": "3a", "text": " ", "value": 0},
+	{"position": "3b", "text": " ", "value": 0},
+	{"position": "3c", "text": " ", "value": 0},
 	]
 
 	$scope.changePlayer = function() {
@@ -26,6 +26,25 @@ angular.module('tictactoeApp', [])
 			$scope.currentPlayer = "X";
 		}
 	}
+
+	$scope.makeMove = function(space) {
+		if(!space.value){
+			console.log("running:", $scope.currentPlayer);
+			space.text = $scope.currentPlayer;
+		}
+	}
+
+	$scope.setPlayerValue = function(space) {
+		if(space.text === "X"){
+			space.value = 1;
+			console.log("space.value = ", space.value);
+		}
+		else if(space.text === "O"){
+			space.value === 2;
+			console.log("$scope.spaces.value = 2");
+		}
+	}
+
 
 })
   
